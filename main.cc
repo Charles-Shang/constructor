48c2c7940ca0a6bb43d6538d2f381d6cb618d38f
Jiaqi Shang <62733535+Charles-Shang@users.noreply.github.com>
2021-07-25 23:04:38 -0400

    Update main.cc
    add divide 3 in mul function

--- main.cc
+++ main.cc
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <iostream>
 
-int mul(int a, int b) { return a * b; }
+int mul(int a, int b) { return a * b / 3; }
 
 int main() { std::cout << "Hello World!!!" << std::endl; }
