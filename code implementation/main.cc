24c62863183ac53f6c05e10d58d45ae62c3f0261
Maruko1337 <78468580+Maruko1337@users.noreply.github.com>
2021-08-06 11:43:37 +0800

    Update main.cc

--- code implementation/main.cc
+++ code implementation/main.cc
@@ -11,6 +11,35 @@
 using std::cin;
 using std::string;
 
-int mul(int a, int b) { return a * b / 3; }
 
-int main() { int a; }
+// print spaces inside tiles (intotal there are 8 spaces)
+void ps() { std::cout << "       "; }
+
+// print the board
+void printBoard() {
+    int vertex = 0;
+    int edge = 0;
+    int tile = 0;
+    // 10 rows of block
+    for (int a = 1; a <= 10; ++a) {
+        // 4 rows in each block
+        for (int b = 1; b <= 4; ++b) {
+            // each row in 5 column
+            for (int c = 0; c <= 5; ++c) {
+                if (a == 1 || (a + c) % 2 == 1) {
+                    if ((a == 1 && (c == 1 || c == 2)) || ()) {
+                        std::cout << "  ";
+                        ps();
+                    }
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    // string name;
+    // cin >> name;
+    // std::cout << guardName(name) << std::endl;
+    printBoard();
+}
